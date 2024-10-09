216613a218dd82b7966d9fd24d9e1b0bba52df15
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-10-09 21:19:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+
+function App() {
+  const [counter,setCounter]=useState(12);
+
+  const handleAdd=()=>{
+    setCounter((prevCounter)=>prevCounter+1);
+    setCounter((prevCounter)=>prevCounter+1);
+    setCounter((prevCounter)=>prevCounter+1);
+    setCounter((prevCounter)=>prevCounter+1);
+  }
+
+  return (
+    <>
+    {console.log("Rendered")}
+      <h1>Counter: {counter}</h1>
+      <h2>body {counter}</h2>
+      <button onClick={handleAdd}>Add</button>
+      <button>Remove</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
