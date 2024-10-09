87003c0e96e63f3ee1fa06944eef10e03a2c3c2b
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-10-09 22:36:31 +0530

    background color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [counter,setCounter]=useState(12);
-
-  const handleAdd=()=>{
-    setCounter((prevCounter)=>prevCounter+1);
-    setCounter((prevCounter)=>prevCounter+1);
-    setCounter((prevCounter)=>prevCounter+1);
-    setCounter((prevCounter)=>prevCounter+1);
-  }
+  const [bgColor, setBgColor] = useState("pink");
 
   return (
-    <>
-    {console.log("Rendered")}
-      <h1>Counter: {counter}</h1>
-      <h2>body {counter}</h2>
-      <button onClick={handleAdd}>Add</button>
-      <button>Remove</button>
-      <p>footer:  {counter}</p>
-    </>
-  )
+    <div
+      className="w-full h-screen duration-200"
+      style={{ background: bgColor }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-4 rounded-full shadow-lg bg-white px-4 py-2">
+          <button
+            className="rounded-full px-2 py-1 shadow-lg bg-red-600 text-red-200"
+            onClick={()=>setBgColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="rounded-full px-2 py-1 shadow-lg bg-green-600 text-green-200"
+            onClick={()=>setBgColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="rounded-full px-2 py-1 shadow-lg bg-blue-600 text-blue-200"
+            onClick={()=>setBgColor("blue")}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
